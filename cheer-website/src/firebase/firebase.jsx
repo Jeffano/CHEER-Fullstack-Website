08d61dfbef4c3dd9c6042495f996f082172327ac
jeffano493 <jeffanojohn@gmail.com>
2024-04-19 16:27:36 -0400

    Final Commit

---
+++ cheer-website/src/firebase/firebase.jsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUPQfAjiIPn-OL_mfaau2U8yYVVNhJ9B0",
+    authDomain: "cheer-website.firebaseapp.com",
+    projectId: "cheer-website",
+    storageBucket: "cheer-website.appspot.com",
+    messagingSenderId: "129129883028",
+    appId: "1:129129883028:web:95b6d80190b09194575b81"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+
+
+export { app, auth };
